572cb67732cbf3b4c52042b1f41287c0f6b0c478
flysky <flysky@nhnedu.com>
2019-10-10 11:36:06 +0900

    스켈레톤 추가

---
+++ main.c
@@ -0,0 +1,22 @@
+void main() {
+	int a, b;
+	string c;
+	int d;
+
+	printf("input: ");
+	scanf("%d %s %d", &a, &c, &b);
+
+	//TODO switch
+
+	printf("result is %d", %d);
+}
+
+int add(int a, int b) {
+	//TODO
+	return 0;
+}
+
+int minus(int a, int b) {
+	//TODO
+	return 0;
+}
